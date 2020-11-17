87cd5cb640017011f2fc4adc29f678288b05dae8
CloudDynasty <32112510+CloudDynasty@users.noreply.github.com>
2020-11-17 22:03:32 +0800

    BackEnd v1. Skeleton of game. No record stored.

--- gomoku/src/main/java/models/Move.java
+++ gomoku/src/main/java/models/Move.java
@@ -8,4 +8,28 @@ public class Move {
 
   private int moveY;
 
+  public Player getPlayer() {
+    return player;
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
+  }
+
+  public int getMoveX() {
+    return moveX;
+  }
+
+  public void setMoveX(int moveX) {
+    this.moveX = moveX;
+  }
+
+  public int getMoveY() {
+    return moveY;
+  }
+
+  public void setMoveY(int moveY) {
+    this.moveY = moveY;
+  }
+
 }
